7c678e923d70dad63ccbb5242a6a4e8455254afb
unknown <dwivediashish22@gmail.com>
2024-10-07 18:36:00 +0530

    fdd

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal,ChangeDetectionStrategy  } from '@angular/core';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { ApiService } from '../../api.service';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule,FormsModule,ToastrModule],
+  imports: [RouterModule,FormsModule,ToastrModule,MatExpansionModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit{
+  readonly panelOpenState = signal(false);
   email: string = '';
   id: number = 0;
   projectId: number = 1;
