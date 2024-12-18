71ba898e008c1fd436d9a6acd58dc36af28093e9
unknown <dwivediashish22@gmail.com>
2024-12-18 15:30:56 +0530

    fdfdf

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,23 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   
 
-sendquery: string='http://localhost:8080/pdawnt/savequery';
-subsribemail: string='http://localhost:8080/pdawnt/emailsubscription';
+
 
 constructor(public http:HttpClient) { }
 
     saveQuery(college:any):Observable<any>{
-      return this.http.post<any>(`${this.sendquery}`,college)
+      return this.http.post<any>(`${environment.sendquery}`,college)
     }
 
     subscribeNews(email: string, projectId: number) {
-       return this.http.post<any>(`${this.subsribemail}`+"?email="+email+"&projectId="+projectId,null)
+       return this.http.post<any>(`${environment.subsribemail}`+"?email="+email+"&projectId="+projectId,null)
     }
 
 }
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+   sendquery:'http://localhost:8080/pdawnt/savequery',
+  subsribemail:'http://localhost:8080/pdawnt/emailsubscription'
+};
